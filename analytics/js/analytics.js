45f3abb3c17609560947c5c3290187d5f956c680
Josh <joshbalsillie@gmail.com>
2019-01-02 18:15:05 -0500

    Simplified code, and removed unnecessary fuctions

--- analytics/js/analytics.js
+++ analytics/js/analytics.js
@@ -22,7 +22,7 @@
 	function afterPageLoad(){
 		// Wait for page to load
 		document.addEventListener("DOMContentLoaded", function( event ){
-			modifyScript();
+			// Listen for event, then perform the following
 		});
 	}
 	function googleAnalytics( id ){
@@ -36,49 +36,8 @@
 		// Add the necessary google script tag to the HTML file
 		var head = document.getElementsByTagName( "head" )[ 0 ];
 		var script = document.createElement( "script" );
-		script.element.setAttribute( "async", "");
-		script.element.setAttribute( "src", "https://www.googletagmanager.com/gtag/js?id=" + id );
+		script.setAttribute( "async", "");
+		script.setAttribute( "src", "https://www.googletagmanager.com/gtag/js?id=" + id );
 		head.appendChild( script );
-	}
-	/*
-	 * SECTION : Functions not specific to this Javascript file
-	 */
-	function retrieveElement( tagName ){
-		// Create an object detailing non-standard aspects of the element's html reference
-		// Note: keep in mind changes to the DOM may affect accuracy
-		function retrieveFilenameFrom( pathName ){
-			// Retrieve the filename of the source value
-			var splitPath = pathName.split( "/" );
-			return splitPath[ splitPath.length - 1 ];
-		}
-		function retrieveHierarchy( htmlElement ){
-			// Retrieve the hierarchical structure of the HTML element
-			var hierarchy = []; // Store html elements
-			var tempElement = htmlElement; // initial value 
-
-			do{
-				hierarchy.unshift( tempElement );
-				tempElement = tempElement.parentElement;
-			}
-			while( tempElement !== null );
-
-			return hierarchy;
-		}
-		var allElements = document.getElementsByTagName( tagName );
-		var theElement = allElements[ allElements.length - 1 ];
-		var thePathName = theElement.src;
-		var theFilename = retrieveFilenameFrom( thePathName );
-		var theName = theFilename.split( "." )[ 0 ];
-		var theHierarchy = retrieveHierarchy( theElement );
-
-		htmlReferences = {
-			// object detailing aspects of the html reference to this file
-			element: theElement, // DOM element
-			filename: theFilename, // Filename with extension
-			hierarchy: theHierarchy, // DOM element hierarchy
-			name: theName, // Filename without extension
-			pathName: thePathName // Path name
-		}
-		return htmlReferences;
 	}
 })();
