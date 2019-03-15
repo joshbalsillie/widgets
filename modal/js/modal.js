5c662720bc6c61aa85c69439951bf277e8ee4489
Josh <joshbalsillie@gmail.com>
2019-03-14 21:22:41 -0400

    Revised Modal
    revised modal to be more modular and simplified HTML use.

---
+++ modal/js/modal.js
@@ -0,0 +1,26 @@
+/*
+ * @author: Joshua Balsillie
+ * @version: 1.0
+ * @since: 2016-09-07
+ */
+
+var modal = {
+	show: function( that ){
+		// Show the content in the modal
+		var content = that.nextElementSibling;
+		
+		content.style.display = "block";
+		
+		window.onclick = function( event ){
+			// After modal is open listen for a click
+			if( event.target == content ){
+				// if the content container is selected...
+				content.style.display = "none"; // close the modal
+			}
+		}
+	},
+	hide: function( that ){
+		// Hide the content in the modal
+		that.parentElement.parentElement.style.display = "none";
+	}
+};
