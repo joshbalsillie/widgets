0931c56c9a880b7463e6f001f06d165bde77f952
Josh <joshbalsillie@gmail.com>
2023-11-03 04:38:12 -0400

    Working prototype
    All 3 thresholds are now working as intended. The tool contains basic styles and functions.

--- widget/colour-library-analyser/js/colour-library-analyser.js
+++ widget/colour-library-analyser/js/colour-library-analyser.js
@@ -9,24 +9,76 @@
 var colourLibraryAnalyser = {
 	// global placeholder object for defining variables and methods for this file
 	get: {
-		colours: function(){
-			let colourElements = document.querySelectorAll('color');
-			//let library = allTags.
+		analysis: function(){
+			// Analyse the library colours against each other and modify the DOM
+			let colourElements = document.querySelectorAll( '#colourLibrary input[type="color"]' );
+			
+			document.querySelectorAll( '#colourLibrary p' ).forEach( function( element ){
+				// remove all previous elements resulting from analysis
+				element.remove();
+			});
+
+			colourElements.forEach( function( element ){
+				// loop through each colour element
+				let currentElement = element;
+				let currentColour = element.nextElementSibling.innerText;
+				var parentContainer = document.createElement( 'div' );
+				var containerA = document.createElement( 'p' );
+				var containerAA = document.createElement( 'p' );
+				var containerAAA = document.createElement( 'p' );
+				containerA.innerHTML = '<span>3:1</span>';
+				containerAA.innerHTML = '<span>4.5:1</span>';
+				containerAAA.innerHTML = '<span>7:1</span>';
+				
+				colourElements.forEach( function( element ){
+					// loop through all colour elements and compare them to the current element in the parent function
+					let compareColour = element.nextElementSibling.innerText;
+					let textInput = element.parentElement.childNodes[ 2 ].value;
+					let contrastRatio = colourLibraryAnalyser.get.contrastRatio( currentColour, compareColour );
+					let compareElement = document.createElement( 'span' );
+					compareElement.style.color = compareColour;
+
+					if( contrastRatio > 3 ){
+						compareElement.innerText = textInput + " (" + compareColour + ") ";
+
+						if( contrastRatio >= 7 ){
+							containerAAA.appendChild( compareElement );
+						}
+						else if( contrastRatio < 7 && contrastRatio > 4.5 ){
+							containerAA.appendChild( compareElement );
+						}
+						else if( contrastRatio < 4.5 && contrastRatio > 3 ){
+							containerA.appendChild( compareElement );
+						}
+					}
+					if( containerA.childElementCount > 0 ){
+						parentContainer.appendChild( containerA );
+					}
+					if( containerAA.childElementCount > 0 ){
+						parentContainer.appendChild( containerAA );
+					}
+					if( containerAAA.childElementCount > 0 ){
+						parentContainer.appendChild( containerAAA );
+					}
+					currentElement.parentElement.appendChild( parentContainer );
+				});
+			});
 		},
 		relativeLuminance: {
 			// Calculate: the relative brightness of any point in a colorspace, normalized to 0 for darkest black and 1 for lightest white
 			// https://www.w3.org/TR/WCAG20/#relativeluminancedef
 			sRGB: function( red, green, blue ){
 				// Calculate the relative luminance of an sRGB colour
-				if( colourLibraryAnalyser.dataTypeIs.number( red ) && colourLibraryAnalyser.dataTypeIs.number( green ) && colourLibraryAnalyser.dataTypeIs.number( blue )){
-					var RsRGB = red / 255;
-					var GsRGB = green / 255;
-					var BsRGB = blue / 255;
-				}
+				let RsRGB = red / 255;
+				let GsRGB = green / 255;
+				let BsRGB = blue / 255;
 
-				let R = ( RsRGB <= 0.03928 ) ? RsRGB / 12.92 : Math.pow(( RsRGB + 0.055 ) / 1.055, 2.4 );
-				let G = ( GsRGB <= 0.03928 ) ? GsRGB / 12.92 : Math.pow(( GsRGB + 0.055 ) / 1.055, 2.4 );
-				let B = ( BsRGB <= 0.03928 ) ? BsRGB / 12.92 : Math.pow(( BsRGB + 0.055 ) / 1.055, 2.4 );
+				function convert( sRGBValue ){
+					return ( sRGBValue <= 0.03928 ) ? sRGBValue / 12.92 : Math.pow(( sRGBValue + 0.055 ) / 1.055, 2.4 );
+				}
+				let R = convert( RsRGB );
+				let G = convert( GsRGB );
+				let B = convert( BsRGB );
 
 				// The relative brightness of any point in a colorspace, normalized to 0 for darkest black and 1 for lightest white
 				// For the sRGB colorspace, the relative luminance of a color is defined as: 
@@ -45,20 +97,11 @@ var colourLibraryAnalyser = {
 		contrastRatio: function( colour1, colour2 ){
 			// Calculate: the contrast ratio between the relative luminance of two colours
 			// https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html
-
-			let rL1 = ( colourLibraryAnalyser.get.relativeLuminance.sRGB( 0, 0, 0 ));
-			//let rL1 = ( colourLibraryAnalyser.get.relativeLuminance.hex( colour1 ));
-
-			if( colourLibraryAnalyser.typeIs.object( typeof colour1 )){
-				
-			}
-			else if( colourLibraryAnalyser.typeIs.string( typeof colour1 )){
-
-			}
+			let rL1 = ( colourLibraryAnalyser.get.relativeLuminance.hex( colour1 ));
+			let rL2 = ( colourLibraryAnalyser.get.relativeLuminance.hex( colour2 ));
 			let L1 = ( rL1 >= rL2 ) ? rL1 : rL2; // L1 is the relative luminance of the lighter of the colors
 			let L2 = ( rL1 >= rL2 ) ? rL2 : rL1; // L2 is the relative luminance of the darker of the colors
-
-			let contrastRatio = ( L1 + 0.05 ) / ( L2 + 0.05 );
+			let contrastRatio = ( L1 + 0.05 ) / ( L2 + 0.05 ); // 1 to 21
 
 			return contrastRatio;
 		},
@@ -66,6 +109,7 @@ var colourLibraryAnalyser = {
 	convert: {
 		hexTo: {
 			sRGB: function( hex ){
+				// Convert hex value to sRGB i.e. #000000 to R:0, G:0, B:0
 				let cleanHex = hex.replaceAll( '#' , '' );
 				cleanHex = cleanHex.replaceAll( ' ' , '' );
 				cleanHex = cleanHex.toUpperCase();
@@ -79,7 +123,6 @@ var colourLibraryAnalyser = {
 					hexArray.forEach( function( hexValue, index ){
 						let numberOrNaN = Number( hexValue );
 						
-
 						if( index % 2 == 0 ){
 							// if index is even array position is odd i.e. #1_3_5_
 							if( !isNaN( numberOrNaN )){
@@ -127,68 +170,53 @@ var colourLibraryAnalyser = {
 			}
 		}
 	},
-	add: {
-		colours: function( quantity ){
+	colours: {
+		add: function( quantity ){
+			// add colours to the end of the libary in the DOM
 			let form = document.getElementById( 'colourLibrary' );
 
 			if( quantity > 0 ){
 				while( quantity > 0 ){
+					let grandParent = document.createElement( 'div' );
 					let parent = document.createElement( 'div' );
 					let colourInput = document.createElement( 'input' );
 					let colourOutput = document.createElement( 'output' );
 					let colourName = document.createElement ( 'input' );
 					let remove = document.createElement( 'input' );
 
-					parent.style.backgroundColor = '#000000';
+					grandParent.style.backgroundColor = '#000000';
 					colourInput.type = 'color';
 					colourInput.setAttribute( 'value', '#000000' );
-					colourInput.setAttribute( 'oninput', 'this.nextElementSibling.value = this.value; this.parentElement.style.backgroundColor = this.value;' );
+					colourInput.setAttribute( 'oninput', 'this.nextElementSibling.value = this.value; this.parentElement.parentElement.style.backgroundColor = this.value;' );
 					colourOutput.setAttribute( 'onchange', 'this.nextElementSibling.value = this.name;' );
 					colourOutput.innerText = '#000000';
 					colourName.type = 'text';
 					colourName.value = 'Colour ' + ( form.childElementCount );
 					remove.type = 'button';
 					remove.setAttribute( 'value', '✕' );
-					remove.setAttribute( 'onClick', 'this.parentElement.remove();' );
+					remove.setAttribute( 'onClick', 'this.parentElement.parentElement.remove();' );
 
 					parent.appendChild( colourInput );
 					parent.appendChild( colourOutput );
 					parent.appendChild( colourName );
 					parent.appendChild( remove );
-					form.insertBefore( parent, form.childNodes[ form.childElementCount ]);
+					grandParent.appendChild( parent );
+					form.insertBefore( grandParent, form.childNodes[ form.childElementCount ]);
 					quantity--;
 				}
 			}
-			else if( quantity < 0 ){
-				while( quantity < 0 ){
+		},
+		remove: function( quantity ){
+			// remove colours from the end of the library in the DOM
+			let form = document.getElementById( 'colourLibrary' );
+
+			if( quantity > 0 ){
+				while( quantity > 0 ){
 					let parent = form.childNodes[ form.childElementCount - 1 ];
 					form.removeChild( parent );
-					quantity++;
+					quantity--;
 				}
 			}
 		}
-	},
-	dataTypeIs: {
-		undefined: function( value ){
-			typeof value == 'undefined' ? true : false;
-		},
-		boolean: function( value ){
-			typeof value == 'boolean' ? true : false;
-		},
-		number: function( value ){
-			typeof value == 'number' ? true : false;
-		},
-		string: function( value ){
-			typeof value == 'string' ? true : false;
-		},
-		symbol: function( value ){
-			typeof value == 'symbol' ? true : false;
-		},
-		function: function( value ){
-			typeof value == 'function' ? true : false;
-		},
-		object: function( value ){
-			typeof value == 'object' ? true : false;
-		}
-	},
+	}
 };
